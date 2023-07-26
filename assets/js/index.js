0954e0f0278a0143d5178bb233e8d46cc77a3c35
Fernando Costa <ferndcosta353@gmail.com>
2023-07-26 20:27:56 -0300

    feat: add 'push endorsement to database' functionality
    Implement the functionality to submit endorsements using the provided form inputs. When the form is submitted, an endorsement object containing the endorsement content, recipient, sender, and default like status is pushed to the Firebase 'endorsements' node.

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,27 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const endorsementsInDB = ref(database, "endorsements");
+
+const endorsementFormEl = document.getElementById('endorsement--form');
+const endorsementInputTextEl = document.getElementById('input-endorsement');
+const endorsementInputToEl = document.getElementById('input-to');
+const endorsementInputFromEl = document.getElementById('input-from');
+
+endorsementFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const endorsementObject = getEndorsementObject();
+
+    push(endorsementsInDB, endorsementObject);
+});
+
+
+function getEndorsementObject() {
+    const endorsementObject = {
+        endorsementContent: endorsementInputTextEl.value,
+        to: endorsementInputToEl.value,
+        from: endorsementInputFromEl.value,
+        isLiked: false,
+    }
+
+    return endorsementObject
+}
