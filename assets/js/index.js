9d2fc93926693c882fc2bd768956e75c339c63c8
Fernando Costa <ferndcosta353@gmail.com>
2023-07-28 17:19:25 -0300

    feat: add function to append endorsements posts from database snapshot
    also add 'likeCount' property to endorsementObject

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,9 +13,11 @@ const endorsementFormEl = document.getElementById('endorsement--form');
 const endorsementInputTextEl = document.getElementById('input-endorsement');
 const endorsementInputToEl = document.getElementById('input-to');
 const endorsementInputFromEl = document.getElementById('input-from');
+const endorsementPostsEl = document.getElementById('endorsement--posts');
 
 endorsementFormEl.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const endorsementObject = getEndorsementObject();
 
     push(endorsementsInDB, endorsementObject);
@@ -28,7 +30,36 @@ function getEndorsementObject() {
         to: endorsementInputToEl.value,
         from: endorsementInputFromEl.value,
         isLiked: false,
+        likeCount: 0
     }
 
     return endorsementObject
+}
+
+onValue(endorsementsInDB, (snapshot) => {
+    if(snapshot.exists()) {
+        const endorsementsArray = Object.entries(snapshot.val());
+
+        for (let i = 0; i < endorsementsArray.length; i++) {
+            const currentEndorsement = endorsementsArray[i];
+            appendPostToEndorsementPostsEl(currentEndorsement);
+        }
+    }
+});
+
+function appendPostToEndorsementPostsEl(post) {
+    const postValue = post[1];
+
+    const newPostEl = document.createElement('article');
+    
+    newPostEl.classList.add('endorsement--post');
+
+    newPostEl.innerHTML = `
+        <span class="post-to">To ${postValue.to}</span>
+        <p class="post-content">${postValue.endorsementContent}</p>
+        <span class="post-from">From ${postValue.from}</span>
+        <span class="post-heart"><i class="fa-solid fa-heart like-icon"></i>${postValue.likeCount}</span>
+    `
+
+    endorsementPostsEl.appendChild(newPostEl);
 }
